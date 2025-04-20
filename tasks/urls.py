b9d074115791b68ecb1248f92337b94881777250
JaroDomcio <281053@student.pwr.edu.pl>
2025-04-20 13:37:03 +0200

    Implemented task editing

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from .views import task_list, create_task, task_detail, delete_task
+from .views import task_list, create_task, task_detail, delete_task, edit_task
 
 urlpatterns = [
     path('',task_list, name='task_list'),
     path('create_task',create_task, name='create_task'),
     path('task/<int:task_id>/',task_detail, name='task_detail'),
     path('task/delete/<int:task_id>/',delete_task, name='delete_task'),
+    path('task/edit/<int:task_id>/',edit_task, name='edit_task')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -11,13 +11,23 @@ def create_task(request):
             return redirect('task_list')
     else: #if request.method == "GET"
         form = TaskForm()
-    return render(request,'create_task.html',{'form':form})
+    return render(request, 'create_or_edit_task.html', {'form':form, 'edit_mode':False})
 
 def delete_task(request,task_id):
     task = get_object_or_404(Task, pk=task_id)
     task.delete()
     return redirect('task_list')
 
+def edit_task(request,task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    if request.method == "POST":
+        form = TaskForm(request.POST,instance = task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance = task)
+    return render(request, 'create_or_edit_task.html', {'form':form, 'edit_mode':True})
 def task_list(request):
     tasks = Task.objects.all()
     return render(request,'task_list.html',{'tasks':tasks})
