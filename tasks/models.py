3b525032e94e941b8ab4d4c2b3f504bd899f43cc
JaroDomcio <281053@student.pwr.edu.pl>
2025-04-20 15:00:09 +0200

    Now description and deadline are optional

--- tasks/models.py
+++ tasks/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank = True)
     completed = models.BooleanField(default=False)
-    deadline = models.DateField()
+    deadline = models.DateField(blank = True, null = True)
 
     def __str__(self):
         return self.title
